b11acbfdd8f8fe13c678ca9966a9112db75eda1d
2022123167a <2022123167@cumhuriyet.edu.tr>
2026-04-03 13:48:30 +0300

    Add root redirect to docs

--- app/main.py
+++ app/main.py
@@ -6,12 +6,19 @@ except ModuleNotFoundError:
     from routes.servers import router as servers_router
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 
 app = FastAPI(
     title="Server Management API",
     version="1.0.0",
 )
+
+
+@app.get("/")
+def root() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
+
 
 app.include_router(servers_router)
 app.include_router(metrics_router)
